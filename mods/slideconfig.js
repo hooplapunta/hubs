a47fa78574b34873b6cda702bb13b895cd17894e
Ron Chew <chewrongkang@gmail.com>
2022-04-29 08:31:53 -0400

    replace jpg with png

--- mods/slideconfig.js
+++ mods/slideconfig.js
@@ -1,14 +1,14 @@
 var slideconfig = {};
 
 slideconfig.slides = [
-"https://hooplapunta.github.io/hubs/mods/slides/Annotation%201.jpg",
-"https://hooplapunta.github.io/hubs/mods/slides/Annotation%202.jpg",
-"https://hooplapunta.github.io/hubs/mods/slides/Annotation%203.jpg",
-"https://hooplapunta.github.io/hubs/mods/slides/Viewpoint%201.jpg",
-"https://hooplapunta.github.io/hubs/mods/slides/Viewpoint%202.jpg",
-"https://hooplapunta.github.io/hubs/mods/slides/Navigation%200.jpg",
-"https://hooplapunta.github.io/hubs/mods/slides/Navigation%201.jpg",
-"https://hooplapunta.github.io/hubs/mods/slides/Navigation%202.jpg",
-"https://hooplapunta.github.io/hubs/mods/slides/Navigation%203.jpg",
-"https://hooplapunta.github.io/hubs/mods/slides/Navigation%204.jpg",
+"https://hooplapunta.github.io/hubs/mods/slides/Annotation%201.png",
+"https://hooplapunta.github.io/hubs/mods/slides/Annotation%202.png",
+"https://hooplapunta.github.io/hubs/mods/slides/Annotation%203.png",
+"https://hooplapunta.github.io/hubs/mods/slides/Viewpoint%201.png",
+"https://hooplapunta.github.io/hubs/mods/slides/Viewpoint%202.png",
+"https://hooplapunta.github.io/hubs/mods/slides/Navigation%200.png",
+"https://hooplapunta.github.io/hubs/mods/slides/Navigation%201.png",
+"https://hooplapunta.github.io/hubs/mods/slides/Navigation%202.png",
+"https://hooplapunta.github.io/hubs/mods/slides/Navigation%203.png",
+"https://hooplapunta.github.io/hubs/mods/slides/Navigation%204.png",
 ];
