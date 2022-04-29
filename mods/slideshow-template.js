5cefc6fc33a6c39b60db7786aac1c3b3b01509df
Ron Chew <chewrongkang@gmail.com>
2022-04-29 08:11:17 -0400

    last attempt

--- mods/slideshow-template.js
+++ mods/slideshow-template.js
@@ -33,11 +33,12 @@
 		// newEntity.setAttribute("shape-helper", { type: "box" });
 		newEntity.setAttribute("set-unowned-body-kinematic", "");
 		newEntity.setAttribute("floaty-object", { // Manages the gravity of an object so that it doesn't fall straight to the floor
-		    modifyGravityOnRelease: true,
+		    // modifyGravityOnRelease: true,
 	    	autoLockOnLoad: true,
 			autoLockOnRelease: true,
-			gravitySpeedLimit: 0,
-			reduceAngularFloat: true
+			// gravitySpeedLimit: 0,
+			reduceAngularFloat: true,
+			releaseGravity: -1
 		});
 		
 		//owned-object-limiter
